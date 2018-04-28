3dfba947f066daf107c4153ebd92359e6f108372
Rafael Herculano <rafa.lj@hotmail.com>
2018-04-28 17:44:45 -0300

    Configurando o componente video details

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom' ;
 import SearchBar from './components/search_bar' ;
 import VideoList from './components/video_list' ;
+import VideoDetail from './components/video_detail' ;
 import YTSearch from 'youtube-api-search' ;
 
 const API_KEY = 'AIzaSyCQ4Nx7SzWeKVQOSoYXZvLTZZX4n7eoGUc' ;
@@ -29,6 +30,7 @@ class App extends Component{
 	render(){
 		return (<div>
 					<SearchBar />
+					<VideoDetail video = {this.state.videos[0]} />
 					<VideoList videos = {this.state.videos} />
 				</div>) ;	
 	}
