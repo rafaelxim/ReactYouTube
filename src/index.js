ad7839b62457edc53361bd2d003fbc5bd721f07d
Rafael Herculano <rafa.lj@hotmail.com>
2018-04-28 15:26:52 -0300

    atualizando o state com os dados da api do youtube

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom' ;
 import SearchBar from './components/search_bar' ;
 import YTSearch from 'youtube-api-search' ;
 
 const API_KEY = 'AIzaSyCQ4Nx7SzWeKVQOSoYXZvLTZZX4n7eoGUc' ;
 
-YTSearch({key:API_KEY, term:'birds'}, function(data){
-	console.log(data);
-})
 
-const App = () => {
-	return (<div>
-				<SearchBar />
-			</div>) ;
+
+class App extends Component{
+
+	constructor(props){
+		super(props) ;
+
+		this.state = { videos: [] }
+
+		YTSearch({key:API_KEY, term:'birds'}, (videos) => {
+
+			this.setState({ videos });
+
+			// A linha acima é a mesma coisa que isso: 
+			// this.setState({videos:videos});
+			// Quando key e value tiverem o mesmo nome posso fazer isto
+		})
+	}	
+
+
+	render(){
+		return (<div>
+					<SearchBar />
+				</div>) ;	
+	}
 }
 
 
