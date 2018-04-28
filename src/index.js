3a383d76bce47c0b6d3e9d9822a208186a606bc3
Rafael Herculano <rafa.lj@hotmail.com>
2018-04-28 15:10:05 -0300

    capturando dados com a api do yt

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom' ;
 import SearchBar from './components/search_bar' ;
+import YTSearch from 'youtube-api-search' ;
 
 const API_KEY = 'AIzaSyCQ4Nx7SzWeKVQOSoYXZvLTZZX4n7eoGUc' ;
+
+YTSearch({key:API_KEY, term:'birds'}, function(data){
+	console.log(data);
+})
 
 const App = () => {
 	return (<div>
